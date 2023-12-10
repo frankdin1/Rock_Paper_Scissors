e1bdebefc7c37c6382da10d9f7ffc5f1a4b9eca9
frankdin <frankdin@yahoo.com>
2023-12-09 20:54:34 -0500

    created array for rps choices, and a function for computer to make its choice.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const choices = ['Rock', 'Paper', 'Scissors']
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+console.log(getComputerChoice());
