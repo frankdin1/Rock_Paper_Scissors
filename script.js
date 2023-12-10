3355906ab29aa6029b6df8a14e510912a7a8e57b
frankdin <frankdin@yahoo.com>
2023-12-10 00:30:04 -0500

    Wrote a function to play a single round of rps.

--- script.js
+++ script.js
@@ -3,7 +3,32 @@
 const choices = ['Rock', 'Paper', 'Scissors']
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
+    return choices[Math.floor(Math.random() * 3)].toLowerCase()
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function singleGameRound(playerChoice, computerChoice) {
+    let gameResult = '';
+    if (playerChoice == computerChoice) {
+        console.log("It's a tie.");
+        singleGameRound(playerChoice, getComputerChoice());
+    }
+
+    if (playerChoice == 'rock' && computerChoice == 'paper') {
+        gameResult = 'You lose. Paper beats rock.'
+    } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        gameResult = 'You win. Rock beats scissors.'
+    } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        gameResult = 'You win. Scissors beat paper.'
+    } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        gameResult = 'You lose. Rock beats scissors.'
+    } else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        gameResult = 'You win. Paper beats rock.'
+    } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        gameResult = 'You lose. Scissors beats paper.'
+    }
+    return gameResult;
+}
+
+console.log(singleGameRound('rock', getComputerChoice()))
