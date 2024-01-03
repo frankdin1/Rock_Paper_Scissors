b63aab4899fdc096f917fea235aeae549f9d36d2
frankdin <frankdin@yahoo.com>
2024-01-02 22:03:42 -0500

    created bannaer for rock, paper, scissors buttons

--- script.js
+++ script.js
@@ -14,14 +14,11 @@ let userScore = 0;
 let compScore = 0;
 
 function getComputerChoice() {
-    console.log("computer choice: " + choices[Math.floor(Math.random() * 3)].toLowerCase())
     computerSelection.innerText = choices[Math.floor(Math.random() * 3)].toLowerCase()
 }
 
 function userSelection() {
     playerSelection.innerText = this.innerText.toLowerCase();
-    console.log("user choice: " + this.innerText);
-    return this.innerText;
 }
 
 function click() {
@@ -71,75 +68,37 @@ function click() {
 
 
 function singleGameRound(playerChoice, computerChoice) {
-    let gameResult = '';
-
     if (playerChoice == computerChoice) {
-        gameResult = 'You tied with Computer.'
     } else if (playerChoice == 'rock' && computerChoice == 'paper') {
-        gameResult = 'You lose. Paper beats rock.'
         compScore++;
     } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
-        gameResult = 'You win. Rock beats scissors.'
         userScore++;
     } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
-        gameResult = 'You win. Scissors beat paper.'
         userScore++;
     } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
-        gameResult = 'You lose. Rock beats scissors.'
         compScore++;
     } else if (playerChoice == 'paper' && computerChoice == 'rock') {
-        gameResult = 'You win. Paper beats rock.'
         userScore++;
     } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
-        gameResult = 'You lose. Scissors beats paper.'
         compScore++;
     }
+
     playerScoreDisplay.innerText = userScore;
     compScoreDisplay.innerText = compScore;
-    if (userScore == 5) {
+
+    if (playerScoreDisplay.innerText == 5) {
         alert("YOU WIN!!!")
         playerScoreDisplay.innerText = 0;
         compScoreDisplay.innerText = 0;
-    } else if (compScore == 5) {
+        userScore = 0;
+        compScore = 0;
+    } else if (compScoreDisplay.innerText == 5) {
         alert("YOU LOSE!!!")
         playerScoreDisplay.innerText = 0;
         compScoreDisplay.innerText = 0;
-    }
-    if (userScore != 5 && compScore != 5) {
-        alert(gameResult);
+        userScore = 0;
+        compScore = 0;
     }
 }
+
 click()
-
-// function game() {
-//     let round = 1;
-//     let w_count = 0;
-//     let l_count = 0;
-//     while (true) {
-//         console.log("Your score: " + w_count)
-//         console.log("Computer score: " + l_count)
-//         let result = '';
-//         result = singleGameRound(choice, getComputerChoice());
-//         if (result == 'w') {
-//             w_count++;
-//             console.log("You win this round.")
-//         } else if (result == 'l') {
-//             l_count++;
-//             console.log("Computer wins this round.")
-//         } else if (result == 't') {
-//             console.log("It is a tie.")
-//         }
-//         if (w_count == 3) {
-//             console.log("You win the game.")
-//             break;
-//         } else if (l_count == 3) {
-//             console.log("Computer wins the game.");
-//             break;
-//         }
-//         round++;
-//     }
-// }
-
-//game()
-
-
