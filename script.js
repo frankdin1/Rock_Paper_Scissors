1c768984a7c631a2aea92d7c3a5debd49e707210
frankdin <frankdin@yahoo.com>
2024-01-02 21:10:45 -0500

    implemented function to compare user and computer choices and display appropriate choices.

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const computerSelection = document.querySelector('#comp-selection')
 const playerScoreDisplay = document.querySelector('#player-score')
 const compScoreDisplay = document.querySelector('#comp-score')
 
-const playerChoice = playerSelection.innerText;
-const computerChoice = computerSelection.innerText;
-const playerScore = playerScoreDisplay.innerText;
-const compScore = compScoreDisplay.innerText;
+let playerChoice = playerSelection.innerText;
+let computerChoice = computerSelection.innerText;
+let playerScore = playerScoreDisplay.innerText;
+let computerScore = compScoreDisplay.innerText;
 
 function getComputerChoice() {
     console.log("computer choice: " + choices[Math.floor(Math.random() * 3)].toLowerCase())
@@ -22,12 +22,9 @@ function getComputerChoice() {
 }
 
 function userSelection() {
-    playerSelection.innerText = this.innerText;
+    playerSelection.innerText = this.innerText.toLowerCase();
     console.log("user choice: " + this.innerText);
-}
-
-function compareHands() {
-    singleGameRound(playerChoice, computerChoice)
+    return this.innerText;
 }
 
 function click() {
@@ -38,6 +35,12 @@ function click() {
     rock.addEventListener(
         'click',
         getComputerChoice
+    )
+    rock.addEventListener(
+        'click',
+        function () {
+            singleGameRound(playerSelection.innerText, computerSelection.innerText)
+        }
     )
     paper.addEventListener(
         'click',
@@ -60,22 +63,32 @@ function click() {
 
 function singleGameRound(playerChoice, computerChoice) {
     let gameResult = '';
+    let userScore = 0;
+    let compScore = 0;
     if (playerChoice == computerChoice) {
         gameResult = 'You tied with Computer.'
     } else if (playerChoice == 'rock' && computerChoice == 'paper') {
         gameResult = 'You lose. Paper beats rock.'
+        compScore++;
     } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
         gameResult = 'You win. Rock beats scissors.'
+        userScore++;
     } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
         gameResult = 'You win. Scissors beat paper.'
+        userScore++;
     } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
         gameResult = 'You lose. Rock beats scissors.'
+        compScore++;
     } else if (playerChoice == 'paper' && computerChoice == 'rock') {
         gameResult = 'You win. Paper beats rock.'
+        userScore++;
     } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
         gameResult = 'You lose. Scissors beats paper.'
+        compScore++;
     }
-    return gameResult[4];
+    playerScore = userScore;
+    computerScore = compScore;
+    alert(gameResult);
 }
 click()
 
